24c12c9b1776b34fe424847031d8df3a20d39d86
Ayelen Martinez <104604365+Ayemartinez@users.noreply.github.com>
2023-06-15 22:48:10 -0300

    actualizacion entrega

---
+++ src/components/navBar.jsx
@@ -0,0 +1,12 @@
+import cart from "../assets/carrito.png"
+export const NavBar = () => <header>
+    <nav>
+        <h1>Yamaha YBR</h1>
+        <ul>
+            <li> <a href="">Repuestos</a></li>
+            <li> <a href="">Cubiertas</a></li>
+            <li> <a href="">Lubricantes</a></li>
+        </ul>
+    </nav>
+    <img src={cart} alt="carrito de compras" /> 0
+</header>
